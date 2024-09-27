ce4a4100f077309ad49ba3ac6ec765703c42bb3c
gustifa <fauzangustifa@gmail.com>
2024-09-27 08:28:21 +0700

    Update Guru

--- resources/views/guru/guru_profile.blade.php
+++ resources/views/guru/guru_profile.blade.php
@@ -1,5 +1,5 @@
-@extends('admin.admin_dashboard')
-@section('admin')
+@extends('guru.guru_dashboard')
+@section('home')
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <div class="page-content">
